b737939bae5c411d8e8aa91d9a90b1c5d787485f
Katarzyna Da Silva <conic916@gmail.com>
2019-06-13 09:54:17 +0200

    Add folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { Router, hashHistory } from 'react-router';
+import  routes from './routes';
 import store from './store/index';
 import DevTools from './devTools';
+//import Navigation from './presentational/navigation.component';
 
-render (
+
+ReactDOM.render(
     <Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
-        <DevTools />
-    </Provider>,
+        <>
+            <Router history={hashHistory} routes={routes} />
+            <DevTools />
+        </>
+    </Provider>, 
     document.getElementById('root')
 );
