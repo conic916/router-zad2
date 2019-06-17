b5aa22025abd1cb63ff84aca5bd801a3fd325245
Katarzyna Da Silva <conic916@gmail.com>
2019-06-17 13:32:52 +0200

    Add modified code

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { Router, hashHistory } from 'react-router';
-import  routes from './routes';
 import store from './store/index';
+import  routes from './routes';
 import DevTools from './devTools';
 //import Navigation from './presentational/navigation.component';
 
@@ -13,6 +13,7 @@ ReactDOM.render(
     <Provider store={store}>
         <>
             <Router history={hashHistory} routes={routes} />
+            
             <DevTools />
         </>
     </Provider>, 
